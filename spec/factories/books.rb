aa2cf1898f8694a4d8af17d395944e0f77861e3c
toshiki <toshimaru0706@gmail.com>
2017-07-18 04:26:27 +0900

    Add book model with controller & views

---
+++ spec/factories/books.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :book do
+    title "MyString"
+    price 1
+  end
+end
---
+++ spec/views/books/new.html.erb_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "books/new", type: :view do
+  before(:each) do
+    assign(:book, Book.new(
+      :title => "MyString",
+      :price => 1
+    ))
+  end
+
+  it "renders new book form" do
+    render
+
+    assert_select "form[action=?][method=?]", books_path, "post" do
+
+      assert_select "input#book_title[name=?]", "book[title]"
+
+      assert_select "input#book_price[name=?]", "book[price]"
+    end
+  end
+end
