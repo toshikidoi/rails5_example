05545e1a7ff9de44642c183f03ecf7c8c4a9c5ee
toshiki <toshimaru0706@gmail.com>
2017-07-18 05:01:47 +0900

    Move launchy's config to capybara support file

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -54,10 +54,4 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-
-  config.after do |example|
-    if example.metadata[:type] == :feature && example.exception.present?
-      save_page Rails.root.join('public', 'capybara.html')
-    end
-  end
 end
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,3 +3,10 @@ require 'capybara/rspec'
 require 'capybara/poltergeist'
 
 Capybara.javascript_driver = :poltergeist
+RSpec.configure do |config|
+  config.after do |example|
+    if example.metadata[:type] == :feature && example.exception.present?
+      save_page Rails.root.join('public', 'capybara.html')
+    end
+  end
+end
